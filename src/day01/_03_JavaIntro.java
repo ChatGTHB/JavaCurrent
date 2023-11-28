be1e841ea254fea4d61569b36f84ae2ddf8d2691
ChatGTHB <cihat.kose@hotmail.com>
2023-11-28 17:20:15 +0100

    Added _03_JavaIntro.java

---
+++ src/day01/_03_JavaIntro.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class _03_JavaIntro {
+    public static void main(String[] args) {
+
+        // Parentheses are immune, so if you get errors, check them first
+
+        System.out.println("Hello World!");  // println("Hello World!")
+
+        // book -> school.library.book -> book in the school library
+        // book -> home.library.book   -> book in home library
+
+        // Mehmet Bey's daughter's child -> mehmet.daughter.child
+        // Ayşe's son's child            -> ayşe.son.child
+    }
+}
