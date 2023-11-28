9383855a7a4b1b49fd1c13dc012c44a49670e36d
ChatGTHB <cihat.kose@hotmail.com>
2023-11-28 17:09:51 +0100

    Added _01_AddComment.java

---
+++ src/day01/_01_AddComment.java
@@ -0,0 +1,21 @@
+package day01;
+
+public class _01_AddComment {
+
+    public static void main(String[] args) { // The main section program starts working from here
+
+        System.out.println("Hello World!"); // Print command to screen
+
+        /*
+         This is commenting multiple lines
+         used to write
+         */
+
+        /**
+         This is commenting multiple lines
+         used to write
+         */
+
+        // The computer does not take into account comment lines
+    }
+}
