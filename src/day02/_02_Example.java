1ea425b85d66efc7713d8a473f1f956fc1f27c1e
ChatGTHB <cihat.kose@hotmail.com>
2023-11-30 21:16:19 +0100

    Added _02_Example.java

---
+++ src/day02/_02_Example.java
@@ -0,0 +1,21 @@
+package day02;
+
+public class _02_Example {
+    public static void main(String[] args) {
+
+        // Question: Write each letter of the word Monday on a separate line.
+
+        System.out.println("M");
+        System.out.println("o");
+        System.out.println("n");
+        System.out.println("d");
+        System.out.println("a");
+        System.out.println("y");
+
+        System.out.println("*************************");
+
+        System.out.println("M\no\nn\nd\na\ny");
+
+        //  \n moves to the bottom line
+    }
+}
