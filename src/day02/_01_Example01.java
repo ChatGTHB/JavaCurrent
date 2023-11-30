b2e1f4cf291de8ac72851519380c458b228b8162
ChatGTHB <cihat.kose@hotmail.com>
2023-11-30 21:12:54 +0100

    Added _01_Example01.java

---
+++ src/day02/_01_Example01.java
@@ -0,0 +1,22 @@
+package day02;
+
+public class _01_Example01 {
+
+    public static void main(String[] args) { // Start
+
+        System.out.println("Friday");  // ln: go to the next line AFTER typing
+        System.out.println("***************");
+
+        System.out.print("F");
+        System.out.print("r");
+        System.out.print("i");
+        System.out.print("d");
+        System.out.print("a");
+        System.out.println("y");
+
+        System.out.println("**********************");
+
+        System.out.println("F\nr\ni\nd\na\ny"); // \n means open NEW line
+
+    } // Stop
+}
