12c46c9f39f51b0f6e7d4c1c4aeb654ca9fb3aab
ChatGTHB <cihat.kose@hotmail.com>
2023-11-30 21:26:49 +0100

    Added _04_QuestionOfTheDay.java

---
+++ src/day02/_04_QuestionOfTheDay.java
@@ -0,0 +1,13 @@
+package day02;
+
+public class _04_QuestionOfTheDay {
+
+    public static void main(String[] args) {
+
+        // Question: Write "Hello ", "World \ /" on the screen
+
+        System.out.println("\"Hello \",  \"World  \\ /\"");
+
+        System.out.println("\"Hello \",  \"World  \\ /  \"    ");
+    }
+}
