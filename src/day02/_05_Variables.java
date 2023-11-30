1ac3b9e6d9ac5a0417517e8833b51c5117592d1c
ChatGTHB <cihat.kose@hotmail.com>
2023-11-30 21:30:57 +0100

    Added _05_Variables.java

---
+++ src/day02/_05_Variables.java
@@ -0,0 +1,24 @@
+package day02;
+
+public class _05_Variables {
+    public static void main(String[] args) {
+
+        /**
+         1- Start
+         2- Read number
+         3- Write number
+         4- Stop
+         */
+
+        // Variable is a memory area that assigns a value to itself and stores it.
+
+        int number; // Integer; A memory area that can store an INTEGER named number.
+
+        number = 5; // I stored the value 5 in the cell named number and assigned it a value
+
+        /**
+         NUMBER=6; //it is case-sensitive, so it should be used with the SAME name.
+         Number=7;
+         */
+    }
+}
