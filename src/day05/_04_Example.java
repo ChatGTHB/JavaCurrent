228cb585356a345f11e44257c4281e4a85921a97
ChatGTHB <cihat.kose@hotmail.com>
2023-12-02 00:46:47 +0100

    Added _04_Example.java

---
+++ src/day05/_04_Example.java
@@ -0,0 +1,16 @@
+package day05;
+
+import java.util.Scanner;
+
+public class _04_Example {
+    public static void main(String[] args) {
+
+        // Take the user's name and surname and print them on the screen
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your name and surname :");
+        String fullName = input.nextLine();
+
+        System.out.println("fullName : " + fullName);
+    }
+}
