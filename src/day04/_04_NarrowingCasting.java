1f69cdc53d5de1a282e68ad8b4b63db29e3adf4a
ChatGTHB <cihat.kose@hotmail.com>
2023-12-01 19:18:06 +0100

    Added _04_NarrowingCasting.java

---
+++ src/day04/_04_NarrowingCasting.java
@@ -0,0 +1,17 @@
+package day04;
+
+public class _04_NarrowingCasting {
+
+    public static void main(String[] args) {
+
+        int total = 6700;      // 2,5 mil
+        byte number = 5;       // 127
+        number = (byte) total;                    // Even if there is no data loss, caution should be exercised
+        System.out.println("number = " + number); // There is data loss
+
+        double oran = 3.7;   // It is also a method used to get the integer part
+        total = (int) oran;  // double -> int
+        System.out.println("total = " + total);
+        // There is data loss, decimal part lost
+    }
+}
