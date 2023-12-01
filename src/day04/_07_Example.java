4b42f749301940e61eebae0f8831731944f424f7
ChatGTHB <cihat.kose@hotmail.com>
2023-12-01 19:18:27 +0100

    Added _07_Example.java

---
+++ src/day04/_07_Example.java
@@ -0,0 +1,20 @@
+package day04;
+
+public class _07_Example {
+
+    public static void main(String[] args) {
+
+        // Find the average of 3 grades given as string ("82","95","56").
+
+        String textGrade1 = "95";
+        String textGrade2 = "82";
+        String textGrade3 = "56";
+
+        double doubleGrade1 = Double.parseDouble(textGrade1);
+        double doubleGrade2 = Double.parseDouble(textGrade2);
+        double doubleGrade3 = Double.parseDouble(textGrade3);
+
+        double average = (doubleGrade1 + doubleGrade2 + doubleGrade3) / 3;
+        System.out.println("average = " + average);
+    }
+}
