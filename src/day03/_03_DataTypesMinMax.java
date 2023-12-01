b913d706e3eafeaae8be18be5152dc47faba1712
ChatGTHB <cihat.kose@hotmail.com>
2023-12-01 09:47:07 +0100

    Added _03_DataTypesMinMax.java

---
+++ src/day03/_03_DataTypesMinMax.java
@@ -0,0 +1,20 @@
+package day03;
+
+public class _03_DataTypesMinMax {
+    public static void main(String[] args) {
+
+        System.out.println("Byte.MIN_VALUE = " + Byte.MIN_VALUE);
+        System.out.println("Byte.MAX_VALUE = " + Byte.MAX_VALUE);
+        System.out.println("Short.MIN_VALUE = " + Short.MIN_VALUE);
+        System.out.println("Short.MAX_VALUE = " + Short.MAX_VALUE);
+        System.out.println("Integer.MAX_VALUE = " + Integer.MIN_VALUE);
+        System.out.println("Integer.MAX_VALUE = " + Integer.MAX_VALUE);
+        System.out.println("Long.MIN_VALUE = " + Long.MIN_VALUE);
+        System.out.println("Long.MAX_VALUE = " + Long.MAX_VALUE);
+
+        System.out.println("Double.MIN_VALUE = " + Double.MIN_VALUE);
+        System.out.println("Double.MAX_VALUE = " + Double.MAX_VALUE);
+        System.out.println("Float.MIN_VALUE = " + Float.MIN_VALUE);
+        System.out.println("Float.MAX_VALUE = " + Float.MAX_VALUE);
+    }
+}
