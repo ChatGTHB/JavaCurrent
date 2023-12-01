ad3dab35b7d70be1f991311455378aae1ccad797
ChatGTHB <cihat.kose@hotmail.com>
2023-12-02 00:41:50 +0100

    Added _02_JavaScanner.java

---
+++ src/day05/_02_JavaScanner.java
@@ -0,0 +1,20 @@
+package day05;
+
+import java.util.Scanner;
+
+public class _02_JavaScanner {
+    public static void main(String[] args) {
+
+        // type  name    first value, new car from cars
+        Scanner input = new Scanner(System.in);
+        // From Computers MyPc = new Computer();
+        // System.in: The task of reading input from the keyboard was given
+
+        System.out.print("Enter a number:");  // It was written so that it would be known what was wanted.
+        int enteredNumber = input.nextInt(); // This command is waiting for a number to be entered
+        // Give me the number you read
+
+        System.out.println("enteredNumber = " + enteredNumber);
+        System.out.println("Finished");
+    }
+}
