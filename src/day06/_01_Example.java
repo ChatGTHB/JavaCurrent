78a9cb8077b12651e0160ce461998948bbc6ea0c
ChatGTHB <cihat.kose@hotmail.com>
2023-12-04 20:49:12 +0100

    Added _01_Example.java

---
+++ src/day06/_01_Example.java
@@ -0,0 +1,28 @@
+package day06;
+
+import java.util.Scanner;
+
+public class _01_Example {
+    public static void main(String[] args) {
+
+        /**
+         Ask the user for the required side (int) lengths of a rectangular field, find its perimeter and area, and print it.
+         perimeter=a+b+a+b    area =a*b
+         */
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter long side: ");
+        int longSide = input.nextInt();
+
+        System.out.print("Enter short side: ");
+        int shortSide = input.nextInt();
+
+        int perimeter = longSide + longSide + shortSide + shortSide;
+        int area = longSide * shortSide;
+
+        System.out.println("The perimeter = " + perimeter);
+        System.out.println("The area = " + area);
+    }
+}
+
