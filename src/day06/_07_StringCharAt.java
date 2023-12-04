392c816a8cbe3c0e82cc29002cea9707c329130e
ChatGTHB <cihat.kose@hotmail.com>
2023-12-04 23:25:35 +0100

    Added _07_StringCharAt.java

---
+++ src/day06/_07_StringCharAt.java
@@ -0,0 +1,15 @@
+package day06;
+
+public class _07_StringCharAt {
+    public static void main(String[] args) {
+
+        // charAt(index) ; Returns the character at the desired point
+        //                      111
+        //            0123456789012
+        String sentence = "Hello World!"; // length 13
+        System.out.println("sentence.length() = " + sentence.length());
+
+        char firstLetter = sentence.charAt(0); // 0 room number, that is, indexed character
+        System.out.println("The first letter = " + firstLetter);
+    }
+}
