b65769495d99d6fab245b92cf1e8e346fc2d009e
ChatGTHB <cihat.kose@hotmail.com>
2023-12-04 23:25:19 +0100

    Added _06_StringLength.java

---
+++ src/day06/_06_StringLength.java
@@ -0,0 +1,15 @@
+package day06;
+
+public class _06_StringLength {
+    public static void main(String[] args) {
+
+        // StringLength -> gives the length of a String (how many letters there are)
+
+        String sentence = "It's very rainy today"; // A space is like a letter.
+
+        int length = sentence.length(); // Returns the number of letters in the value in the sentence.
+
+        System.out.println("Length = " + length);
+        System.out.println("Length2 = " + sentence.length()); // The second option
+    }
+}
