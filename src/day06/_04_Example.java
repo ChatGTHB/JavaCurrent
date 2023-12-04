dc953a1cf8738044891dce4c5f4928c8513a671b
ChatGTHB <cihat.kose@hotmail.com>
2023-12-04 21:18:31 +0100

    Added _04_Example.java

---
+++ src/day06/_04_Example.java
@@ -0,0 +1,20 @@
+package day06;
+
+import java.util.Scanner;
+
+public class _04_Example {
+    public static void main(String[] args) {
+
+        /**
+         Ask the user whether they are a student; If the answer is yes, answer True;
+         otherwise, answer False and print the answer received.
+         */
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Are you student ? =");
+        boolean answer = input.nextBoolean(); // True,true or False,false
+
+        System.out.println("answer = " + answer);
+    }
+}
