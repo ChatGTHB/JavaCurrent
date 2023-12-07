51235ce4d1f54ea949beea3aca80642711838b0c
ChatGTHB <cihat.kose@hotmail.com>
2023-12-07 23:48:02 +0100

    Added _05_StringContains.java

---
+++ src/day07/_05_StringContains.java
@@ -0,0 +1,19 @@
+package day07;
+
+public class _05_StringContains {
+    public static void main(String[] args) {
+
+        // contains : Returns whether the searched letter/s are in the string or not
+        // Returns the result in boolean
+
+        String sentence = "Hello World!";
+
+        boolean isThere = sentence.contains("o");  // true
+        System.out.println("isThere = " + isThere);
+
+        System.out.println("isThere o= " + sentence.contains("o"));   // 2.yöntem
+        System.out.println("isThere lo =" + sentence.contains("lo")); // true
+        System.out.println("isThere z =" + sentence.contains("z"));   // false
+        System.out.println("isThere aü =" + sentence.contains("aü")); // false
+    }
+}
