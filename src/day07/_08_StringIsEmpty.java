6f9d0e73aefb87c9ab91be7ed768514c8d48d276
ChatGTHB <cihat.kose@hotmail.com>
2023-12-07 23:59:11 +0100

    Added _08_StringIsEmpty.java

---
+++ src/day07/_08_StringIsEmpty.java
@@ -0,0 +1,15 @@
+package day07;
+
+public class _08_StringIsEmpty {
+    public static void main(String[] args) {
+
+        // IsEmpty : Is there one or more characters in a string or not?
+        // boolean
+
+        String s1 = "Hello";
+        String s2 = "";
+
+        System.out.println("s1.isEmpty() = " + s1.isEmpty()); // s1 isEmpty  false
+        System.out.println("s2.isEmpty() = " + s2.isEmpty()); // true
+    }
+}
