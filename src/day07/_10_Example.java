a9473b34b78fcf8974f3c5b3da6a1071ce7cd294
ChatGTHB <cihat.kose@hotmail.com>
2023-12-08 00:08:06 +0100

    Added _10_Example.java

---
+++ src/day07/_10_Example.java
@@ -0,0 +1,26 @@
+package day07;
+
+import java.util.Scanner;
+
+public class _10_Example {
+    public static void main(String[] args) {
+
+        // Separate the name and surname of a name and surname entered at once and print them separately. (name and surname only)
+        //  For example, Joseph Burns -> Print Joseph separately and Burns separately
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Name and surname: ");
+        String nameSurname = input.nextLine();
+
+        int spaceIndex = nameSurname.indexOf(" ");
+
+        String name = nameSurname.substring(0, spaceIndex);
+        String surname = nameSurname.substring(spaceIndex + 1);
+
+        System.out.println("name = " + name);
+        System.out.println("surname = " + surname);
+    }
+}
+
+
