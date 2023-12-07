a73c343843e4a0320d96a16254bbea828f590c66
ChatGTHB <cihat.kose@hotmail.com>
2023-12-08 00:50:24 +0100

    Added _04_JavaComparisonOperators.java

---
+++ src/day09/_04_JavaComparisonOperators.java
@@ -0,0 +1,16 @@
+package day09;
+
+public class _04_JavaComparisonOperators {
+    public static void main(String[] args) {
+
+        int a = 50;
+        int b = 50;
+
+        System.out.println(" a > b " + (a > b));   // false : Is a greater than b?
+        System.out.println(" a >= b " + (a >= b)); // true : Is a greater than or equal to b??
+        System.out.println(" a < b " + (a < b));   // false : Is a less than b?
+        System.out.println(" a <= b " + (a <= b)); // true : Is a less than equal to b??
+        System.out.println(" a == b " + (a == b)); // true : Is a equal to b?
+        System.out.println(" a != b " + (a != b)); // false : Is a different from b??
+    }
+}
