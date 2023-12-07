caed77986caf926560fb9b474b8a0bb1fd73dead
ChatGTHB <cihat.kose@hotmail.com>
2023-12-07 23:48:02 +0100

    Added _04_StringConcat.java

---
+++ src/day07/_04_StringConcat.java
@@ -0,0 +1,19 @@
+package day07;
+
+public class _04_StringConcat {
+    public static void main(String[] args) {
+
+        // concat : concatenates one string to another
+
+        String s1 = "Hello";
+        String s2 = "World!";
+
+        System.out.println("unified version= " + s1 + s2);
+        System.out.println("unified version= " + s1.concat(s2)); // unified version
+        System.out.println("unified version= " + s1.concat(" ").concat(s2));
+        System.out.println("unified version= " + s1.concat(" " + s2));
+
+        System.out.println("s1 = " + s1); // The value of S1 has not changed
+        System.out.println("s2 = " + s2); // The value of S2 has not changed
+    }
+}
