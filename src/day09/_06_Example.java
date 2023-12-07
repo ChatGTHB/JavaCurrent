68c9302f481fa76c0230b07629ba50b0057a02f6
ChatGTHB <cihat.kose@hotmail.com>
2023-12-08 00:56:34 +0100

    Added _06_Example.java

---
+++ src/day09/_06_Example.java
@@ -0,0 +1,22 @@
+package day09;
+
+import java.util.Scanner;
+
+public class _06_Example {
+    public static void main(String[] args) {
+
+        // Print whether an entered number is an odd number or not.
+        // true veya false
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Number= ");
+        int number = input.nextInt();
+
+        int remaining = number % 2; // The module always gives the rest
+
+        System.out.println("Is odd number? " + (remaining == 1));  // Method1
+
+        boolean isOdd = (remaining == 1);
+        System.out.println("isOdd = " + isOdd);              // Method2
+    }
+}
