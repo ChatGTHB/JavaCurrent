cc394397614c5bb84a7e9578edbf0b083a38501a
ChatGTHB <cihat.kose@hotmail.com>
2023-12-08 22:26:26 +0100

    Added _02_JavaIf.java

---
+++ src/day10/_02_JavaIf.java
@@ -0,0 +1,21 @@
+package day10;
+
+import java.util.Scanner;
+
+public class _02_JavaIf {
+    public static void main(String[] args) {
+
+        //  According to a student's grade entered,
+        //  if it is 50 and greater than 50, you pass,
+        //  if it is smaller, you fail.
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Student grade= ");
+        int studentGrade = input.nextInt();
+
+        if (studentGrade >= 50) System.out.println("You've passed, congratulations!");
+
+        if (studentGrade < 50) System.out.println("Sorry, you're left");
+    }
+}
