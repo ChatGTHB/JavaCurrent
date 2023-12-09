b1651a044a9b5700c547f0c31a35fe5616513382
ChatGTHB <cihat.kose@hotmail.com>
2023-12-09 15:23:58 +0100

    Added _06_JavaIf.java

---
+++ src/day10/_06_IfElse.java
@@ -0,0 +1,29 @@
+package day10;
+
+import java.util.Scanner;
+
+public class _06_IfElse {
+    public static void main(String[] args) {
+
+        //  According to a student's grade entered,
+        //  if it is 50 and greater than 50, you pass,
+        //  if it is smaller, you fail.
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your grade:");
+        int studentGrade = input.nextInt();
+
+        // Method 1: The computer gets tired in vain
+        if (studentGrade >= 50) System.out.println("You've passed, congratulations!");
+
+        if (studentGrade < 50) {
+            System.out.println("Sorry, you're stuck");
+            System.out.println("We are waiting for you to summer school");
+        }
+
+        // Method 2: Should be
+        if (studentGrade >= 50) System.out.println("You've passed, congratulations!");
+        else // If the up is not a must
+            System.out.println("Sorry, you're stuck");
+    }
+}
