cd7e23710b9ef5fab2ad3dfb6f2b2db8889f568b
ChatGTHB <cihat.kose@hotmail.com>
2023-12-10 19:46:03 +0100

    Added _07_IfElse.java

---
+++ src/day10/_07_IfElse.java
@@ -0,0 +1,27 @@
+package day10;
+
+import java.util.Scanner;
+
+public class _07_IfElse {
+    public static void main(String[] args) {
+
+        // Print whether an entered number is odd or even.
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Number: ");
+        int number = input.nextInt();
+
+        // Method 1
+
+        int remain = number % 2;
+
+        if (remain == 0) System.out.println("Even number");
+        else System.out.println("Odd number");
+
+        // Method 2
+
+        if (number % 2 == 0) System.out.println("Even number");
+        else System.out.println("Odd number");
+    }
+}
