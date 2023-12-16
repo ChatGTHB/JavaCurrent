920b569eb336c4380d2b34c903308603ae65f8e9
ChatGTHB <cihat.kose@hotmail.com>
2023-12-16 19:41:56 +0100

    Added _06_JavaTernary.java

---
+++ src/day11/_06_JavaTernary.java
@@ -0,0 +1,22 @@
+package day11;
+
+import java.util.Scanner;
+
+public class _06_JavaTernary {
+    public static void main(String[] args) {
+
+        /**
+         *    Question: Print whether an entered number is zero,
+         *    negative or positive with the Ternary operator.
+         */
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Number: ");
+        int number = input.nextInt();
+
+        String result = number == 0 ? "Zero" : (number > 0 ? "Pozitive" : "Negative");
+
+        System.out.println("Result = " + result);
+    }
+}
