ed430215f73d86a17a1750ce71d27f20f944de39
ChatGTHB <cihat.kose@hotmail.com>
2023-12-16 19:41:48 +0100

    Added _05_JavaTernary.java

---
+++ src/day11/_05_JavaTernary.java
@@ -0,0 +1,25 @@
+package day11;
+
+import java.util.Scanner;
+
+public class _05_JavaTernary {
+    public static void main(String[] args) {
+
+        /**
+         Question: If the number entered is greater than 50, print the value "Greater",
+         if not, print the "Not" value on the screen. Do it with the ternary operator.
+         */
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        String result = number > 50 ? "Great" : "Not Great";
+
+        System.out.println("result = " + result);
+
+        // int intResult= number > 50 ? 1 : 0;
+        // boolean booleanResult= number > 50 ? true : false;
+    }
+}
