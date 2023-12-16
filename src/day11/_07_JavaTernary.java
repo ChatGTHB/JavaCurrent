b24403e891213a22cb451a0f0106c8bfd2ca3e05
ChatGTHB <cihat.kose@hotmail.com>
2023-12-16 19:42:05 +0100

    Added _07_JavaTernary.java

---
+++ src/day11/_07_JavaTernary.java
@@ -0,0 +1,36 @@
+package day11;
+
+import java.util.Scanner;
+
+public class _07_JavaTernary {
+    public static void main(String[] args) {
+
+        // Parking fees: (Do it with the ternary operator.)
+        // 50 TL for up to 3 hours, 100 TL for up to 5 hours, 120 TL for more than 5 hours.
+        // Print the fee by taking the number of hours left from the user
+
+        // If hour is < 3, write 50 TL
+        // If  hour is >= 3 and hour is < 5, write 100 TL
+        // If the hour is >= 5, write 120 TL
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Hour: ");
+        int hour = input.nextInt();
+
+        // As string    ? -> if      : -> else
+        String stringFee = (hour < 3 ? "50 tl" : (hour < 5 ? "100tl" : "120tl"));
+        System.out.println("stringFee = " + stringFee);
+
+        // As int
+        int fee = (hour < 3 ? 50 : (hour < 5 ? 100 : 120));
+        System.out.println("fee = " + fee + " tl");
+    }
+}
+
+
+
+
+
+
+
+
