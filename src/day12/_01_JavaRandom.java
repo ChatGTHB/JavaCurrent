494acbec8c3d05c643946d6df1c67ac7439f2818
ChatGTHB <cihat.kose@hotmail.com>
2023-12-17 19:18:47 +0100

    Added _01_JavaRandom.java

---
+++ src/day12/_01_JavaRandom.java
@@ -0,0 +1,20 @@
+package day12;
+
+public class _01_JavaRandom {
+    public static void main(String[] args) {
+
+        // Math.Random() : Produces a double number. Produces from 0 to 0.9999999999.
+        // System.out.println(Math.random());
+
+        // I want a random integer between 0-10
+
+        int randomNumber = (int) (Math.random() * 25);
+        System.out.println("randomNumber = " + randomNumber);
+    }
+}
+
+//        0 - 0,9999999999
+//
+//        Up to 10    -> 9 max
+//        Up to 100   -> 99 max
+//        Up to 25    -> 24 max
