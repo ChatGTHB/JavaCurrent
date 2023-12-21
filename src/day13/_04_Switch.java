11973c99dfc4494c9aec0dcdfd8909925d8781c0
ChatGTHB <cihat.kose@hotmail.com>
2023-12-21 20:14:47 +0100

    Updated _04_Switch.java

--- src/day13/_04_Switch.java
+++ src/day13/_04_Switch.java
@@ -4,50 +4,79 @@ import java.util.Scanner;
 
 public class _04_Switch {
     public static void main(String[] args) {
+
         // Girilen bir Ay numarasına göre ayın gün sayısını yazdırınız.
 
-        Scanner oku=new Scanner(System.in);
+        Scanner oku = new Scanner(System.in);
         System.out.print("Ay no=");
-        int ayNo=oku.nextInt();
+        int ayNo = oku.nextInt();
 
-        switch (ayNo){
-            case 1: System.out.println(31); break;
-            case 2 : System.out.println(28);break;
-            case 3 : System.out.println(31);break;
-            case 4 : System.out.println(30);break;
-            case 5 : System.out.println(31);break;
-            case 6 : System.out.println(30);break;
-            case 7 : System.out.println(31);break;
-            case 8 : System.out.println(31);break;
-            case 9 : System.out.println(30);break;
-            case 10 : System.out.println(31);break;
-            case 11 : System.out.println(30);break;
-            case 12 : System.out.println(31);break;
-            default : System.out.println("Hatalı Ay No");
+        switch (ayNo) {
+            case 1:
+                System.out.println(31);
+                break;
+            case 2:
+                System.out.println(28);
+                break;
+            case 3:
+                System.out.println(31);
+                break;
+            case 4:
+                System.out.println(30);
+                break;
+            case 5:
+                System.out.println(31);
+                break;
+            case 6:
+                System.out.println(30);
+                break;
+            case 7:
+                System.out.println(31);
+                break;
+            case 8:
+                System.out.println(31);
+                break;
+            case 9:
+                System.out.println(30);
+                break;
+            case 10:
+                System.out.println(31);
+                break;
+            case 11:
+                System.out.println(30);
+                break;
+            case 12:
+                System.out.println(31);
+                break;
+            default:
+                System.out.println("Hatalı Ay No");
         }
 
         //2.Yol
-        switch (ayNo){
-            case 2 : System.out.println(28);break;
+        switch (ayNo) {
+            case 2:
+                System.out.println(28);
+                break;
 
-            case 3 :
-            case 1 :
-            case 5 :
-            case 7 :
-            case 8 :
-            case 10 :
-            case 12 : System.out.println(31);break;
+            case 3:
+            case 1:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println(31);
+                break;
 
-            case 4 :
-            case 6 :
-            case 9 :
-            case 11 : System.out.println(30);break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println(30);
+                break;
 
-            default : System.out.println("Hatalı Ay No");
+            default:
+                System.out.println("Hatalı Ay No");
         }
-
-
-
-
     }
 }
