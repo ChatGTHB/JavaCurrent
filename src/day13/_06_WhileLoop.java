f445367b94287b34076c56add66847a329f5ce4e
ChatGTHB <cihat.kose@hotmail.com>
2023-12-21 20:15:42 +0100

    Updated _06_WhileLoop.java

--- src/day13/_06_WhileLoop.java
+++ src/day13/_06_WhileLoop.java
@@ -4,26 +4,26 @@ import java.util.Scanner;
 
 public class _06_WhileLoop {
     public static void main(String[] args) {
-        //Girilen  5 tam sayının toplamının sonucunu yazdırınız
 
-        //döngünü içinde hangi adımlar olacak
+        //Girilen 5 tam sayının toplamının sonucunu yazdırınız
+
+        // döngünün içinde hangi adımlar olacak
         // sayi iste
         // okuna sayı toplam
         // sayac++
         // peki döngü şartı ?
 
-        Scanner oku=new Scanner(System.in);
+        Scanner oku = new Scanner(System.in);
 
-        int sayac=0, toplam=0;
+        int sayac = 0, toplam = 0;
 
-        while (sayac <5) {
+        while (sayac < 5) {
             System.out.print("Sayi giriniz=");
             int sayi = oku.nextInt();
 
-            toplam=toplam+sayi;
+            toplam = toplam + sayi;
             sayac++;
         }
-
         System.out.println("toplam = " + toplam);
     }
 }
