e024f340dd17123d51399ce24daca615b72fc5b0
ChatGTHB <cihat.kose@hotmail.com>
2023-12-21 20:13:43 +0100

    Updated _02_Switch.java

--- src/day13/_02_Switch.java
+++ src/day13/_02_Switch.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 public class _02_Switch {
     public static void main(String[] args) {
+
         // Kullanıcının girdiği gün numarasına karşılık gelen günün adınız yazınız.
 
-        Scanner oku=new Scanner(System.in);
+        Scanner oku = new Scanner(System.in);
         System.out.print("Gün no=");
-        int gunNo=oku.nextInt();
+        int gunNo = oku.nextInt();
 
 //        if ( gunNo==1)
 //            System.out.println("Pazartesi");
@@ -16,15 +17,31 @@ public class _02_Switch {
 //            if (gunNo == 2)
 //                System.out.println("Salı");
 
-        switch (gunNo){
-            case 1: System.out.println("Pazartesi"); break;
-            case 2: System.out.println("Salı"); break;
-            case 3: System.out.println("Çarşamba"); break;
-            case 4: System.out.println("Perşembe"); break;
-            case 5: System.out.println("Cuma"); break;
-            case 6: System.out.println("Cumartesi"); break;
-            case 7: System.out.println("Pazar"); break;
-            default:System.out.println("Hatalı giriş yaptınız"); break;
+        switch (gunNo) {
+            case 1:
+                System.out.println("Pazartesi");
+                break;
+            case 2:
+                System.out.println("Salı");
+                break;
+            case 3:
+                System.out.println("Çarşamba");
+                break;
+            case 4:
+                System.out.println("Perşembe");
+                break;
+            case 5:
+                System.out.println("Cuma");
+                break;
+            case 6:
+                System.out.println("Cumartesi");
+                break;
+            case 7:
+                System.out.println("Pazar");
+                break;
+            default:
+                System.out.println("Hatalı giriş yaptınız");
+                break;
         }
     }
 }
