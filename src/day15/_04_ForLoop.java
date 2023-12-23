e1048a9fac359d2e18a3a2531267a9b2fd60b0ff
ChatGTHB <cihat.kose@hotmail.com>
2023-12-23 12:27:37 +0100

    Added _04_ForLoop.java

---
+++ src/day15/_04_ForLoop.java
@@ -0,0 +1,22 @@
+package day15;
+
+public class _04_ForLoop {
+    public static void main(String[] args) {
+
+        // 2 ler çarpım tablosunu , aşağıdaki şekilde
+        // olarak yazdırınız.
+        //    2 x 1 = 2
+        //    2 x 2 = 4
+        //    2 x 3 = 6
+        //    2 x 4 = 8
+        //    2 x 5 = 10
+        //    2 x 6 = 12
+        //    2 x 7 = 14
+        //    2 x 8 = 16
+        //    2 x 9 = 18
+        //    2 x 10 = 20
+
+        for (int i = 1; i <= 10; i++)
+            System.out.println("2 x " + i + " = " + (2 * i));
+    }
+}
