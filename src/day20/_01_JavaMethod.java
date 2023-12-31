2bc0924013fc80c5ff436239032cd191de6e1713
ChatGTHB <cihat.kose@hotmail.com>
2023-12-31 15:06:11 +0100

    Added _01_JavaMethod.java

---
+++ src/day20/_01_JavaMethod.java
@@ -0,0 +1,29 @@
+package day20;
+
+public class _01_JavaMethod { //fonksiyon, Method
+
+    // yazılabildiği yerler
+
+    public static void main(String[] args) {
+
+        // amaç: main in içi sade ve okunabilir olsun,
+        // tekrarlanan kodları 1 kere yaz, istediğin kadar çalıştır Math.Random();
+        diziDoldur();
+        diziYazdir();
+        diziTeklerBul();
+
+    }
+
+    // ya da burada yazılırlar
+    public static void diziDoldur() { // metod, fonksiyon
+        // kodlar yazılıyor
+    }
+
+    public static void diziYazdir() { // metod, fonksiyon
+        // kodlar yazılıyor
+    }
+
+    public static void diziTeklerBul() { // metod, fonksiyon
+        // kodlar yazılıyor
+    }
+}
