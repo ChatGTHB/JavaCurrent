eb3f9da5efdb97bd40a25ea922a666dd4d307a0d
ChatGTHB <cihat.kose@hotmail.com>
2024-01-13 21:51:50 +0100

    Added new example about static and non static methods

---
+++ src/day27/_01_Example/Utility.java
@@ -0,0 +1,14 @@
+package day27._01_Example;
+
+public class Utility {
+
+    static int randomSayiVer2(int sinir) {
+        return (int) (Math.random() * sinir);
+    }
+
+    int randomSayiVer(int sinir) {
+        return (int) (Math.random() * sinir);
+    }
+
+
+}
