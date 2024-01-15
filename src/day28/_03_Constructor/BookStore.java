7af7c047f55375cc0dab6cb0d5be3e532da389dc
ChatGTHB <cihat.kose@hotmail.com>
2024-01-15 23:53:40 +0100

    Edited

--- src/day28/_03_Constructor/BookStore.java
+++ src/day28/_03_Constructor/BookStore.java
@@ -4,11 +4,11 @@ public class BookStore {
 
     public static void main(String[] args) {
 
-        Book kit1 = new Book();
-        kit1.name = "Harry Poter";
-        kit1.author = "JK Rowling";
-        kit1.publishYear = 1997;
+        Book book1 = new Book();
+        book1.name = "Harry Poter";
+        book1.author = "JK Rowling";
+        book1.publishYear = 1997;
 
-        System.out.println(kit1);
+        System.out.println(book1);
     }
 }
