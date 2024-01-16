65cc8c27a1f11aea7d216f0988ec68b47325f102
ChatGTHB <cihat.kose@hotmail.com>
2024-01-16 23:06:57 +0100

    Added new file about access modifiers

--- src/day29/_01_ClassErisimleri/PaketIki/DigerPaketMain.java
+++ src/day29/_01_ClassErisimleri/PaketIki/DigerPaketMain.java
@@ -4,11 +4,10 @@ import day29._01_ClassErisimleri.PaketBir.PublicErisim;
 
 public class DigerPaketMain {
     public static void main(String[] args) {
-        PublicErisim pe=new PublicErisim();
-        // DefaultErisim de=new DefaultErisim();
-        // diger paketlerden default belirtecli class a
-        // erişilemez.yanlızca kendi paketindekiler erişebilir
 
+        PublicErisim publicErisim = new PublicErisim();
+        // DefaultErisim defaultErisim=new DefaultErisim();
+        // Diğer paketlerden default belirtecli classa erişilemez.
+        // Yalnızca kendi paketindekiler erişebilir
     }
-
 }
