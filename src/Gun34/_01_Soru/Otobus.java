473ed5ab41b76dbf3979e65f2f6163dfc57f56a4
ChatGTHB <cihat.kose@hotmail.com>
2024-01-29 21:39:31 +0100

    Added a new example about OOP

---
+++ src/Gun34/_01_Soru/Otobus.java
@@ -0,0 +1,23 @@
+package Gun34._01_Soru;
+
+public class Otobus extends Arac {
+    private int yolcuKapasitesi;
+
+    public Otobus(String renk, int motorHacmi, String marka, int yolcuKapasitesi) {
+        super(renk, motorHacmi, marka);
+        setYolcuKapasitesi(yolcuKapasitesi);
+    }
+
+    public int getYolcuKapasitesi() {
+        return yolcuKapasitesi;
+    }
+
+    public void setYolcuKapasitesi(int yolcuKapasitesi) {
+        this.yolcuKapasitesi = yolcuKapasitesi;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", yolcuKapasitesi='" + this.yolcuKapasitesi + '\'';
+    }
+}
