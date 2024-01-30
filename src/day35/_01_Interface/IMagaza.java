845d22f792084c9f22fb9dca3cc6ef8a957514c5
ChatGTHB <cihat.kose@hotmail.com>
2024-01-30 21:59:10 +0100

    Added Interface examples

---
+++ src/day35/_01_Interface/IMagaza.java
@@ -0,0 +1,16 @@
+package day35._01_Interface;
+
+public interface IMagaza {
+
+    void bilgileriAl();
+
+    void krediSkorunuHesapla();
+
+    void krediSenecekleriniGoster();
+
+    void belgeleriImzalat();
+
+    void krediyiKullandir();
+
+    void krediyiKullandir2();
+}
---
+++ src/day35/_01_Interface/TrendYol.java
@@ -0,0 +1,33 @@
+package day35._01_Interface;
+
+public class TrendYol implements IMagaza {
+    @Override
+    public void bilgileriAl() {
+
+    }
+
+    @Override
+    public void krediSkorunuHesapla() {
+
+    }
+
+    @Override
+    public void krediSenecekleriniGoster() {
+
+    }
+
+    @Override
+    public void belgeleriImzalat() {
+
+    }
+
+    @Override
+    public void krediyiKullandir() {
+
+    }
+
+    @Override
+    public void krediyiKullandir2() {
+
+    }
+}
