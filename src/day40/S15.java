952a72a1480e58d07aaa81dc5887d8430cb07757
ChatGTHB <cihat.kose@hotmail.com>
2024-02-19 15:46:28 +0100

    Added two OCA question examples

---
+++ src/day40/S15.java
@@ -0,0 +1,18 @@
+package day40;
+
+public class S15 {
+
+    public static void main(String[] args) {
+
+        int[] array;
+        array = new int[2];
+
+        //int array[]=new int[2];
+        //int[] array={10,20};
+        //int[] array=new int[]{10,20};
+
+        array[0] = 10;
+        array[1] = 20;
+        System.out.println(array[0] + ":" + array[1]);
+    }
+}
---
+++ src/day40/S21.java
@@ -0,0 +1,16 @@
+package day40;
+
+public class S21 {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4}; // indexleri : 0,1,2,3
+
+        int i = 0;
+
+        do {
+            System.out.print(arr[i] + " "); // 1,2,3,4
+            i++;
+        } while (i < arr.length + 1); // i<5   -> 1,2,3,4
+    }
+}
