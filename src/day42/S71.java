4f2e1a6183bcb6aa7f207a5455e37f2b32c0dd5c
ChatGTHB <cihat.kose@hotmail.com>
2024-03-05 19:34:52 +0100

    Added two OCA question examples

---
+++ src/day42/S71.java
@@ -0,0 +1,23 @@
+package day42;
+
+public class S71 {
+
+    public static void main(String[] args) {
+
+        int[][] arr = new int[2][4];
+        arr[0] = new int[]{1, 3, 5, 7};
+        arr[1] = new int[]{1, 3};
+
+        System.out.println("arr.length = " + arr.length); // satır sayısı : 2
+        System.out.println("arr[0].length = " + arr[0].length); // 1.(0 ind) satırın uzunluğu: 4
+        System.out.println("arr[1].length = " + arr[1].length); // 2.(1 ind) satırın uzunluğu: 2
+
+        for (int[] a : arr) { // a önce 1. satır sonra 2.satırı gönderir
+
+            for (int i = 0; i < arr.length; i++) { // her bir satırı 2 ye kadar yani 0 ve 1 ind için yazacak
+                System.out.print(a[i] + " ");
+            }
+            //System.out.println();
+        }
+    }
+}
---
+++ src/day42/S82.java
@@ -0,0 +1,26 @@
+package day42;
+
+public class S82 {
+
+    public static void main(String[] args) {
+
+        String[][] chs = new String[2][]; // 2 satır , sutunlar belli değil
+        chs[0] = new String[2];  //2 tane sütun  , 0.satır 2 sutun olsun
+        chs[1] = new String[5];  //5 tane sütun ,  1.satır 5 sütun olsun
+        int i = 97;
+
+        for (int a = 0; a < chs.length; a++) {  //2
+            for (int b = 0; b < chs.length; b++) { //2
+                chs[a][b] = "" + i;
+                i++;
+            }
+        }
+
+        for (String[] ca : chs) {
+            for (String c : ca) {
+                System.out.print(c + " ");
+            }
+            System.out.println();
+        }
+    }
+}
