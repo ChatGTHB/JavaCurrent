eefa83a46c145c45be567274be9b21c4921cb9c6
ChatGTHB <cihat.kose@hotmail.com>
2024-06-04 22:04:22 +0200

    Edited

--- src/day04/_06_Example.java
+++ src/day04/_06_Example.java
@@ -4,17 +4,16 @@ public class _06_Example {
 
     public static void main(String[] args) {
 
-        String donateion1="700"; // string, word
-        String donation2="500"; // string, word
+        String donateion1 = "700"; // string, word
+        String donation2 = "500"; // string, word
 
-        System.out.println(donateion1+donation2); // 700500
+        System.out.println(donateion1 + donation2); // 700500
 
         // I want to sum them as numbers then it is necessary to convert String -> int.
-        int figureDonation1= Integer.parseInt(donateion1);  // Double.parseDouble
-        int figureDonation2= Integer.parseInt(donation2);   // Parse : means to organize and transform
-        int totalDonations=figureDonation1+figureDonation2;
+        int figureDonation1 = Integer.parseInt(donateion1);  // Double.parseDouble
+        int figureDonation2 = Integer.parseInt(donation2);   // Parse : means to organize and transform
+        int totalDonations = figureDonation1 + figureDonation2;
 
-        System.out.println(figureDonation1+figureDonation2);
+        System.out.println(figureDonation1 + figureDonation2);
     }
 }
-
