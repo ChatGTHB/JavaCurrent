a8452e3caa3f88055c6d8b3496a43123db01d83a
ChatGTHB <cihat.kose@hotmail.com>
2024-07-21 18:03:25 +0200

    Edited

--- src/day19/_04_Java2DArrays.java
+++ src/day19/_04_Java2DArrays.java
@@ -3,26 +3,29 @@ package day19;
 import java.util.Arrays;
 
 public class _04_Java2DArrays {
-
     public static void main(String[] args) {
+        int[][] table = new int[2][3]; // 2 rows and 3 columns array
 
-        int[][] tablo = new int[2][3];
+        // Different row sizes can be defined using this way
+        int[][] table2 = {
+                {2, 8},
+                {22, 33, 44, 5, 6, 7, 8},
+                {3, 4, 5, 6, 6, 7}
+        };
 
-        int[][] tablo2 = // farklı satır boyutları ancak bu tanımlama şekli ile oluşturulabilir.
-                {{2, 8}, {22, 33, 44, 5, 6, 7, 8}, {3, 4, 5, 6, 6, 7}};
+        // table2[0] = new int[9]; // You can also redefine any row with a different size if needed.
 
-        //tablo2[0]=new int[9];  // ya da herhangi bir satırı yeniden istediğiniz boyutla verebilirsiniz.
-
-        for (int i = 0; i < tablo2.length; i++) {
-
-            for (int j = 0; j < tablo2[i].length; j++)
-                System.out.print(tablo2[i][j] + "\t");
-
+        // Print each element of table2
+        for (int i = 0; i < table2.length; i++) {
+            for (int j = 0; j < table2[i].length; j++) {
+                System.out.print(table2[i][j] + "\t");
+            }
             System.out.println();
         }
 
-        System.out.println("0.satır = " + Arrays.toString(tablo2[0]));
-        System.out.println("1.satır = " + Arrays.toString(tablo2[1]));
-        System.out.println("2.satır = " + Arrays.toString(tablo2[2]));
+        // Print each row of table2
+        System.out.println("Row 0 = " + Arrays.toString(table2[0]));
+        System.out.println("Row 1 = " + Arrays.toString(table2[1]));
+        System.out.println("Row 2 = " + Arrays.toString(table2[2]));
     }
 }
