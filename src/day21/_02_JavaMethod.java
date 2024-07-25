1f2dbc9080fb46c0e45f0a518db8801f163d3d99
ChatGTHB <cihat.kose@hotmail.com>
2024-07-25 19:19:51 +0200

    Edited

--- src/day21/_02_JavaMethod.java
+++ src/day21/_02_JavaMethod.java
@@ -2,30 +2,28 @@ package day21;
 
 public class _02_JavaMethod {
     public static void main(String[] args) {
+        int n1 = 4;
+        int n2 = 12;
+        int n3 = 12;
 
-        int s1 = 4;
-        int s2 = 12;
-        int s3 = 12;
+        int result = findSum(n1, n2);
+        int result2 = findSum(n1, n2, n3);
+        String result3 = findSum("ismet", "temur");
 
-        int sonuc = toplamBul(s1, s2);
-        int sonuc2 = toplamBul(s1, s2, s3);
-        String sonuc3 = toplamBul("ismet", "temur");
-
-        System.out.println("sonuc = " + sonuc);
-        System.out.println("sonuc2 = " + sonuc2);
-        System.out.println("sonuc3 = " + sonuc3);
+        System.out.println("result = " + result);
+        System.out.println("result2 = " + result2);
+        System.out.println("result3 = " + result3);
     }
 
-    public static String toplamBul(String ad, String soyad) {
-        return ad + soyad;
+    public static String findSum(String firstName, String lastName) {
+        return firstName + lastName;
     }
 
-    public static int toplamBul(int s1, int s2) {
-        return s1 + s2;
+    public static int findSum(int n1, int n2) {
+        return n1 + n2;
     }
 
-    public static int toplamBul(int sayi1, int sayi2, int sayi3) {
-        return sayi1 + sayi2 + sayi3;
+    public static int findSum(int n1, int n2, int n3) {
+        return n1 + n2 + n3;
     }
-
 }
