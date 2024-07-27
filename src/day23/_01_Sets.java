bcb17dfca84caeb538da458487b84c7173536ad6
ChatGTHB <cihat.kose@hotmail.com>
2024-07-27 19:12:06 +0200

    Edited

--- src/day23/_01_Sets.java
+++ src/day23/_01_Sets.java
@@ -3,32 +3,30 @@ package day23;
 import java.util.ArrayList;
 
 public class _01_Sets {
-
     public static void main(String[] args) {
+        int number = 5;
 
-        int sayi = 5;
+        int[] array = new int[10];      // 1D
+        int[][] table = new int[2][5];  // 2D
 
-        int[] dizi = new int[10];      // 1D
-        int[][] tablo = new int[2][5]; // 2D
-
-        ArrayList<Integer> liste = new ArrayList<>();               // 1D
-        ArrayList<ArrayList<Integer>> listeler = new ArrayList<>(); // 2D
+        ArrayList<Integer> list = new ArrayList<>();               // 1D
+        ArrayList<ArrayList<Integer>> lists = new ArrayList<>();   // 2D
 
         /********************************************************/
 
-        // Javacım senden şu ana kadar lazım durumda uygun değişkenleri aldım, teşekkür ederim.
-        // fakat bana şöyle dizi lazım; içine verileri attığımda tekrar eden veri gelirse EKLEMESİN,
-        // 2.cisi, bu dizi
-        // a) istersem, ne eklersem ekliyim, herzaman içindekileri SIRALI tutsun,
-        // b) istersem, eklenme sırasında tutsun
-        // c) istersem, en hızlı çalışmak için, kendisi istediği sırada tutsun
+        // Java, so far I've taken the necessary variables from you, thank you.
+        // but I need an array that, when I add data to it, if duplicate data comes in, DO NOT ADD it,
+        // Secondly, this array
+        // a) if I want, no matter what I add, always keep the contents SORTED,
+        // b) if I want, keep the insertion order,
+        // c) if I want, to work as fast as possible, keep it in any order it prefers
 
-        // Tek tek eleman eleman işlemler için değil de, işlem yapılacağı zaman, bütün elemanların işleme
-        // alınmasına uygun olan bu toplu işlem dizisine SET denir.
-        // a) TreeSet       -> her zaman sıralı
-        // b) LinkedHashSet -> eklenme sırasına göre
-        // c) HashSet       -> hızlı çalışsın sırası önemli değil
+        // These arrays suitable for bulk operations where all elements are processed together
+        // rather than individual element operations are called SETs.
+        // a) TreeSet       -> always sorted
+        // b) LinkedHashSet -> in the order of insertion
+        // c) HashSet       -> works quickly, order does not matter
 
-        // Bunlarda INDEX yok
+        // There is no INDEX in these
     }
 }
--- src/day23/_02_Sets.java
+++ src/day23/_02_Sets.java
@@ -3,11 +3,8 @@ package day23;
 import java.util.HashSet;
 
 public class _02_Sets {
-
     public static void main(String[] args) {
-
         HashSet<Integer> hashSet = new HashSet<>();
-
         hashSet.add(1);
         hashSet.add(2);
         hashSet.add(38);
@@ -16,9 +13,9 @@ public class _02_Sets {
 
         System.out.println("hashSet = " + hashSet);
 
-                        // Bakalım eklenecek mi
-        hashSet.add(4); // Eklemek isteseniz de SET ler tekrar eklemez
-        hashSet.add(2); // Eklemek isteseniz de SET ler tekrar eklemez
+        // Let's see if it will be added
+        hashSet.add(4); // Even if you try to add, SETs do not add duplicates
+        hashSet.add(2); // Even if you try to add, SETs do not add duplicates
 
         System.out.println("hashSet = " + hashSet);
     }
