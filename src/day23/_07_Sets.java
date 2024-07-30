25dd1471eb3aef724a90bef0c331a9af5ac1696f
ChatGTHB <cihat.kose@hotmail.com>
2024-07-30 17:29:40 +0200

    Edited

--- src/day23/_07_Sets.java
+++ src/day23/_07_Sets.java
@@ -3,9 +3,8 @@ package day23;
 import java.util.HashSet;
 
 public class _07_Sets {
-
     public static void main(String[] args) {
-
+        // Creating the first set (setA)
         HashSet<Integer> setA = new HashSet<>();
         setA.add(1);
         setA.add(2);
@@ -13,6 +12,7 @@ public class _07_Sets {
         setA.add(4);
         setA.add(5);
 
+        // Creating the second set (setB)
         HashSet<Integer> setB = new HashSet<>();
         setB.add(4);
         setB.add(5);
@@ -22,22 +22,22 @@ public class _07_Sets {
         System.out.println("setA = " + setA);
         System.out.println("setB = " + setB);
 
-        // Birleştirme
-        HashSet<Integer> birlesikHali = new HashSet<>();
-        birlesikHali.addAll(setA);
-        birlesikHali.addAll(setB);
-        System.out.println("Birleşik Hali = " + birlesikHali);
+        // Union of setA and setB
+        HashSet<Integer> unionSet = new HashSet<>();
+        unionSet.addAll(setA);
+        unionSet.addAll(setB);
+        System.out.println("Union of setA and setB = " + unionSet);
 
-        // Farkını bulma
-        HashSet<Integer> farki = new HashSet<>();
-        farki.addAll(setA);
-        farki.removeAll(setB);
-        System.out.println("Farkı = " + farki);
+        // Difference between setA and setB
+        HashSet<Integer> differenceSet = new HashSet<>();
+        differenceSet.addAll(setA);
+        differenceSet.removeAll(setB);
+        System.out.println("Difference between setA and setB = " + differenceSet);
 
-        // Kesişim bulma
-        HashSet<Integer> ortakElemanlar = new HashSet<>();
-        ortakElemanlar.addAll(setA);
-        ortakElemanlar.retainAll(setB);
-        System.out.println("Ortak Elemanlar = " + ortakElemanlar);
+        // Intersection of setA and setB
+        HashSet<Integer> intersectionSet = new HashSet<>();
+        intersectionSet.addAll(setA);
+        intersectionSet.retainAll(setB);
+        System.out.println("Intersection of setA and setB = " + intersectionSet);
     }
 }
