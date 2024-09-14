a66262f9622cdb4c1a1ec980ca2d16cb4ab23800
ChatGTHB <cihat.kose@hotmail.com>
2024-09-14 18:30:43 +0200

    Update _01_JavaLocalDate

--- src/day37/_01_JavaLocalDate.java
+++ src/day37/_01_JavaLocalDate.java
@@ -5,42 +5,41 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
 public class _01_JavaLocalDate {
-
     public static void main(String[] args) {
-        // LocalDate Sadece gun ay yıl bilgisini tutar
-        LocalDate tarih = LocalDate.now();
+        // LocalDate stores only the date information like day, month, and year
+        LocalDate date = LocalDate.now();
 
-        System.out.println("tarih = " + tarih);
-        System.out.println("tarih.getYear() = " + tarih.getYear());
-        System.out.println("tarih.getMonth() = " + tarih.getMonth()); // ayın adını
-        System.out.println("getMonth().getValue() = " + tarih.getMonth().getValue());//ay no
-        System.out.println("tarih.getDayOfMonth() = " + tarih.getDayOfMonth()); // aynı kaçıncı günü
-        System.out.println("tarih.getDayOfWeek() = " + tarih.getDayOfWeek());
-        System.out.println("getDayOfWeek().getValue() = " + tarih.getDayOfWeek().getValue());
-        System.out.println("tarih.getDayOfYear() = " + tarih.getDayOfYear());
+        System.out.println("date = " + date);
+        System.out.println("date.getYear() = " + date.getYear());
+        System.out.println("date.getMonth() = " + date.getMonth()); // returns the name of the month
+        System.out.println("getMonth().getValue() = " + date.getMonth().getValue());// returns the month number
+        System.out.println("date.getDayOfMonth() = " + date.getDayOfMonth()); // which day of the month it is
+        System.out.println("date.getDayOfWeek() = " + date.getDayOfWeek());
+        System.out.println("getDayOfWeek().getValue() = " + date.getDayOfWeek().getValue());
+        System.out.println("date.getDayOfYear() = " + date.getDayOfYear());
 
-        // kendisindeki hazır formatlar
-        System.out.println("ISO_DATE = " + tarih.format(DateTimeFormatter.ISO_DATE));
-        System.out.println("SHORT = " + tarih.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)));
-        System.out.println("MEDIUM = " + tarih.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)));
-        System.out.println("LONG = " + tarih.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG)));
-        System.out.println("FULL = " + tarih.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)));
+        // built-in formats
+        System.out.println("ISO_DATE = " + date.format(DateTimeFormatter.ISO_DATE));
+        System.out.println("SHORT = " + date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)));
+        System.out.println("MEDIUM = " + date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)));
+        System.out.println("LONG = " + date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG)));
+        System.out.println("FULL = " + date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)));
 
-        // İstediğim formatta gösterim
-        System.out.println(" *********  İstediğimiz formatta gösterim **********");
-        DateTimeFormatter ozelFormat1=DateTimeFormatter.ofPattern("dd/MM/YYYY"); // tarih şablonu
-        System.out.println("tarih dd/MM/YYYY = " + tarih.format(ozelFormat1));
+        // Displaying in custom formats
+        System.out.println(" *********  Displaying in custom formats **********");
+        DateTimeFormatter customFormat1 = DateTimeFormatter.ofPattern("dd/MM/YYYY"); // date pattern
+        System.out.println("date dd/MM/YYYY = " + date.format(customFormat1));
 
-        DateTimeFormatter ozelFormat2=DateTimeFormatter.ofPattern("d.M.YY"); // tarih şablonu
-        System.out.println("tarih d.M.YY = " + tarih.format(ozelFormat2));
+        DateTimeFormatter customFormat2 = DateTimeFormatter.ofPattern("d.M.YY"); // date pattern
+        System.out.println("date d.M.YY = " + date.format(customFormat2));
 
-        DateTimeFormatter of3=DateTimeFormatter.ofPattern("EEEE dd.MM.YYYY"); // tarih şablonu
-        System.out.println("tarih EEEE dd.MM.YYYY = " + tarih.format(of3));
+        DateTimeFormatter of3 = DateTimeFormatter.ofPattern("EEEE dd.MM.YYYY"); // date pattern
+        System.out.println("date EEEE dd.MM.YYYY = " + date.format(of3));
 
-        DateTimeFormatter of4=DateTimeFormatter.ofPattern("EE dd.MM.YYYY"); // tarih şablonu
-        System.out.println("tarih EE dd.MM.YYYY = " + tarih.format(of4));
+        DateTimeFormatter of4 = DateTimeFormatter.ofPattern("EE dd.MM.YYYY"); // date pattern
+        System.out.println("date EE dd.MM.YYYY = " + date.format(of4));
 
-        DateTimeFormatter of5=DateTimeFormatter.ofPattern("MMMM EEEE dd.MM.YYYY"); // tarih şablonu
-        System.out.println("tarih MMMM EEEE dd.MM.YYYY = " + tarih.format(of5));
+        DateTimeFormatter of5 = DateTimeFormatter.ofPattern("MMMM EEEE dd.MM.YYYY"); // date pattern
+        System.out.println("date MMMM EEEE dd.MM.YYYY = " + date.format(of5));
     }
 }
