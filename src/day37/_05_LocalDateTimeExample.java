2836ff13a0f9a66055dcd7ff555f2de2cec05afa
ChatGTHB <cihat.kose@hotmail.com>
2024-09-14 18:37:15 +0200

    Update _05_LocalDateTimeExample

---
+++ src/day37/_05_LocalDateTimeExample.java
@@ -0,0 +1,23 @@
+package day37;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class _05_LocalDateTimeExample {
+    public static void main(String[] args) {
+        // LocalDateTime stores both date and time information
+
+        LocalDateTime currentDateTime = LocalDateTime.now(); // Getting current date and time
+        System.out.println("Current DateTime = " + currentDateTime);
+
+        // Accessing individual components of the date and time
+        System.out.println("Year = " + currentDateTime.getYear());
+        System.out.println("Hour = " + currentDateTime.getHour());
+        System.out.println("Month = " + currentDateTime.getMonth());
+        // You can retrieve other parts of the date and time as well
+
+        // Defining a custom format for the date and time
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy hh:mm");
+        System.out.println("Formatted DateTime = " + currentDateTime.format(formatter));
+    }
+}
