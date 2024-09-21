06ad9f21967e215b57fcdf61d3878518b266e3e2
ChatGTHB <cihat.kose@hotmail.com>
2024-09-21 14:32:03 +0200

    Update _02_JavaRuntimeException

--- src/day39/_02_JavaRuntimeException.java
+++ src/day39/_02_JavaRuntimeException.java
@@ -4,25 +4,28 @@ import java.util.Scanner;
 
 public class _02_JavaRuntimeException {
     public static void main(String[] args) {
-        System.out.println("Program çalışmaya başladı");
+        System.out.println("The program has started");
 
-        Scanner oku = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        try {  // olay yeri inceleme, hata bölgesi
-            System.out.print("Sayı1=");
-            int sayi1 = oku.nextInt();
+        try {
+            // Try block: this is where you try to execute code that may cause an exception
 
-            System.out.print("Sayı2=");
-            int sayi2 = oku.nextInt();
+            System.out.print("Number1 = ");
+            int number1 = scanner.nextInt();  // Taking the first number as input
 
-            int bolum = sayi1 / sayi2;
-            System.out.println("bolum = " + bolum);
-        } catch (Exception hata) // oluşan hatayı hata isimli değişkene atıyor
-        {
-            System.out.println("hata oluştu");
-            System.out.println(hata);
+            System.out.print("Number2 = ");
+            int number2 = scanner.nextInt();  // Taking the second number as input
+
+            // Division operation that might cause an ArithmeticException (e.g., divide by zero)
+            int divisionResult = number1 / number2;
+            System.out.println("division result = " + divisionResult);
+
+        } catch (Exception error) {
+            // Catch block: if any exception occurs in the try block, this block will execute
+            System.out.println("An error occurred");
+            System.out.println(error);  // Print the error message
         }
-
-        System.out.println("Program bitti");
+        System.out.println("The program has ended");
     }
 }
