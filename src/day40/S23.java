8bf7dcc1df1e66005f08bf96cff4f03bc6efc06d
ChatGTHB <cihat.kose@hotmail.com>
2024-09-28 10:43:00 +0200

    Update S23

--- src/day40/S23.java
+++ src/day40/S23.java
@@ -1,23 +1,21 @@
 package day40;
 
 public class S23 {
-
     public static void main(String[] args) {
-
-        int[] stack = {10, 20, 30}; // 0,1,2 indexler
+        int[] stack = {10, 20, 30}; // indices: 0, 1, 2
         int size = 3;
         int idx = 0;
 
 //        while (idx < size){  // idx < 3
 //            idx++;
-//        } // idx 3
+//        } // idx becomes 3
 
         do {
             idx++;
         } while (idx < size - 1);
 
-        //idx 2 olmalı
+        // idx should be 2
         System.out.println("idx = " + idx);
-        System.out.println("The Top elemen : " + stack[idx]);  // 30
+        System.out.println("The top element: " + stack[idx]);  // 30
     }
 }
