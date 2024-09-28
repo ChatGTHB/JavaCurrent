cc12f6d0a71905e1c0a3319822819e9728565d3d
ChatGTHB <cihat.kose@hotmail.com>
2024-09-28 10:42:13 +0200

    Update S21

--- src/day40/S21.java
+++ src/day40/S21.java
@@ -1,16 +1,14 @@
 package day40;
 
 public class S21 {
-
     public static void main(String[] args) {
-
-        int[] arr = {1, 2, 3, 4}; // indexleri : 0,1,2,3
+        int[] arr = {1, 2, 3, 4}; // indices: 0, 1, 2, 3
 
         int i = 0;
 
         do {
-            System.out.print(arr[i] + " "); // 1,2,3,4
+            System.out.print(arr[i] + " "); // 1, 2, 3, 4
             i++;
-        } while (i < arr.length + 1); // i<5   -> 1,2,3,4
+        } while (i < arr.length + 1); // i < 5 -> 1, 2, 3, 4
     }
 }
