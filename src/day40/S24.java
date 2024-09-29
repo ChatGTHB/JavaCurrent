8a5ddb5af47e24b5addfd778ca7da4c8ebc9af13
ChatGTHB <cihat.kose@hotmail.com>
2024-09-29 12:47:05 +0200

    Update S24

--- src/day40/S24.java
+++ src/day40/S24.java
@@ -1,12 +1,10 @@
 package day40;
 
 public class S24 {
-
     public static void main(String[] args) {
+        String[] strs = new String[2]; // null
 
-        String[] strs = new String[2]; //null
-
-        // String ifadelerin dizilerinde default değer NULL
+        // Default value in String arrays is NULL
         for (int i = 0; i < strs.length; i++) {
             System.out.print(strs[i]);
         }
@@ -14,10 +12,10 @@ public class S24 {
         int idx = 0;
 
         for (String s : strs) {
-            //strs[idx] = strs[idx] + "element"; bu olurdu
-            strs[idx].concat(" element " + idx); // null.concat  olamaz
+            // strs[idx] = strs[idx] + " element"; this would work
+            strs[idx].concat(" element " + idx); // null.concat can't be done
             idx++;
-        } //concat : var olan değerinin üzerine ekle
+        } // concat: appends to the existing value
 
         for (idx = 0; idx < strs.length; idx++) {
             System.out.print(strs[idx]);
