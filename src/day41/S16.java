16ef7c192a791e521bba8e8b3e821b24baf0c421
ChatGTHB <cihat.kose@hotmail.com>
2024-10-18 21:38:35 +0200

    Update S16

--- src/day41/S16.java
+++ src/day41/S16.java
@@ -1,24 +1,21 @@
 package day41;
 
 public class S16 {
-
     public static void main(String[] args) {
-
         int cnt = 0;
-        String[][] arr = {{"A", "B", "C"},   // 0.satır
-                {"D", "E"}         // 1.satır
+        String[][] arr = {{"A", "B", "C"},   // row 0
+                {"D", "E"}         // row 1
         };
 
-        for (int i = 0; i < arr.length; i++) {  //0
-
-            for (int j = 0; j < arr[i].length; j++) {  // 0,1,2
-                System.out.print(arr[i][j] + " "); // A,B,C,D,E
+        for (int i = 0; i < arr.length; i++) {  // row 0
+            for (int j = 0; j < arr[i].length; j++) {  // columns 0,1,2
+                System.out.print(arr[i][j] + " "); // A, B, C, D, E
 
                 if (arr[i][j].equals("B")) {
-                    continue; //etkisiz eleman
+                    continue; // no effect
                 }
             }
-            continue; //etkisiz eleman
+            continue; // no effect
         }
     }
 }
