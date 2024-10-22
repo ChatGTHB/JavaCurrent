57ab6bf345b04e39f56a2624ccd67083156ea655
ChatGTHB <cihat.kose@hotmail.com>
2024-10-22 23:24:56 +0200

    Update Q17

--- src/day41/Q17.java
+++ src/day41/Q17.java
@@ -1,16 +1,14 @@
 package day41;
 
-public class S17 {
-
+public class Q17 {
     public static void main(String[] args) {
-
         int[][] arr = new int[2][4];
 
-        arr[0] = new int[]{1, 3, 5, 7};
-        arr[1] = new int[]{1, 3};
+        arr[0] = new int[]{1, 3, 5, 7}; // row 0
+        arr[1] = new int[]{1, 3};       // row 1
 
+        // Traditional for loop
         for (int i = 0; i < arr.length; i++) {
-
             for (int j = 0; j < arr[i].length; j++)
                 System.out.print(arr[i][j] + " ");
 
@@ -19,10 +17,10 @@ public class S17 {
 
         System.out.println("-----------");
 
-        for (int[] satir : arr) {
-
-            for (int i : satir)
-                System.out.print(i + " ");
+        // Enhanced for loop
+        for (int[] row : arr) {
+            for (int element : row)
+                System.out.print(element + " ");
 
             System.out.println();
         }
