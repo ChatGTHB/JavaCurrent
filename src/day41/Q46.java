c2dd3d0e28e64edd82e8c71140f355a591f6c904
ChatGTHB <cihat.kose@hotmail.com>
2024-10-22 23:26:11 +0200

    Update Q46

--- src/day41/Q46.java
+++ src/day41/Q46.java
@@ -1,16 +1,14 @@
 package day41;
 
-public class S46 {
-
+public class Q46 {
     public static void main(String[] args) {
-
         String[] names = {"Thomas", "Peter", "Joseph"};
         String[] pwd = new String[3];
 
         int idx = 0;
         try {
             for (String n : names) { // Thomas, Peter
-                pwd[idx] = n.substring(2, 6); // 0->omas
+                pwd[idx] = n.substring(2, 6); // 0 -> omas
                 idx++;
             }
         } catch (Exception e) {
@@ -18,6 +16,6 @@ public class S46 {
         }
 
         for (String p : pwd)
-            System.out.println(p); //omas, null,null
+            System.out.println(p); // omas, null, null
     }
 }
