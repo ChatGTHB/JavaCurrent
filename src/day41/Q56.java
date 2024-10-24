586b222d7c2abf21b190a3708eeae33a5042fd68
ChatGTHB <cihat.kose@hotmail.com>
2024-10-24 22:35:34 +0200

    Update Q56

--- src/day41/Q56.java
+++ src/day41/Q56.java
@@ -1,21 +1,19 @@
 package day41;
 
-public class S56 {
-
+public class Q56 {
     public static void main(String[] args) {
-
         String[] names = {"Thomas", "Peter", "Joseph"};
         String[] pwd = new String[3];
         int idx = 0;
 
         try {
-            for (String n : names) {  //Thomas 012345, Peter 01234
-                pwd[idx] = n.substring(2, 6); // 2 dahil, 5 dahil 2-5 dahil alıyor
+            for (String n : names) {  // Thomas 012345, Peter 01234
+                pwd[idx] = n.substring(2, 6); // includes 2, includes 5 (grabs characters from 2 to 5)
                 System.out.println(pwd[idx]); // omas
                 idx++;
             }
         } catch (Exception e) {
-            System.out.println("Invalid Name"); //Invalid Name
+            System.out.println("Invalid Name"); // Invalid Name
         }
     }
 }
