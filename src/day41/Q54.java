fef1a5c34f2df9becb6df872a0a8f0dc08b1200f
ChatGTHB <cihat.kose@hotmail.com>
2024-10-24 22:34:06 +0200

    Update Q54

--- src/day41/Q54.java
+++ src/day41/Q54.java
@@ -1,22 +1,19 @@
 package day41;
 
-public class S54 {
-
+public class Q54 {
     public static void main(String[] args) {
-
         int cnt = 0;
         String[][] arr = {{"A", "B", "C"}, {"D", "E"}};
 
         for (int i = 0; i < arr.length; i++) {
-
             for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + " "); //A,B,D,E
+                System.out.print(arr[i][j] + " "); // A, B, D, E
 
                 if (arr[i][j].equals("B")) {
                     break;
                 }
             }
-            continue; //etkisiz
+            continue; // no effect
         }
     }
 }
