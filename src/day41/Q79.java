43c59fe8d620e063fc04711b752cda7a77b96886
ChatGTHB <cihat.kose@hotmail.com>
2024-11-01 20:35:18 +0100

    Update Q79

--- src/day41/Q79.java
+++ src/day41/Q79.java
@@ -1,20 +1,17 @@
 package day41;
 
-public class S79 {
-
+public class Q79 {
     public static void main(String[] args) {
-
         String[] arr = {"A", "B", "C", "D"};
 
         for (int i = 0; i < arr.length; i++) {
-
             System.out.print(arr[i] + " "); // ABCD
 
             if (arr[i].equals("D")) {
-                System.out.print("Work Done");  //Work Done
+                System.out.print("Work Done");  // Work Done
                 break;
             }
-            continue; // kendisinden sonra gelnleri çalıştırmaz
+            continue; // skips the remaining code after this point
         }
     }
 }
