f79fb1fa55be202b7a7a525e37448915e4636f92
ChatGTHB <cihat.kose@hotmail.com>
2024-11-04 22:59:14 +0100

    Update Q39

--- src/day42/Q39.java
+++ src/day42/Q39.java
@@ -1,9 +1,7 @@
 package day42;
 
-public class S39 {
-
+public class Q39 {
     public static void main(String[] args) {
-
         String[][] shirts = new String[2][2];
         shirts[0][0] = "red";
         shirts[0][1] = "blue";
