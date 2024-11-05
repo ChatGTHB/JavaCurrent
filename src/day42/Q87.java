e8ac82dd2ee5e059e3dfd65d493a8ea8877674de
ChatGTHB <cihat.kose@hotmail.com>
2024-11-05 22:43:16 +0100

    Update Q87

--- src/day42/Q87.java
+++ src/day42/Q87.java
@@ -1,9 +1,7 @@
 package day42;
 
-public class S87 {
-
+public class Q87 {
     public static void main(String[] args) {
-
         int[][] n = {{1, 3}, {2, 4}};
 
         for (int i = n.length - 1; i >= 0; i--) { // i: 1, 0
