80d5c4cad55f7a369579a7fa9f5114f7b506fdd0
ChatGTHB <cihat.kose@hotmail.com>
2024-11-05 22:42:58 +0100

    Update Q71

--- src/day42/Q71.java
+++ src/day42/Q71.java
@@ -1,9 +1,7 @@
 package day42;
 
-public class S71 {
-
+public class Q71 {
     public static void main(String[] args) {
-
         int[][] arr = new int[2][4];
         arr[0] = new int[]{1, 3, 5, 7};
         arr[1] = new int[]{1, 3};
