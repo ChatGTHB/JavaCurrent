f9f4df8ed76cffad7c8d3a89cb19291897c3e6bf
ChatGTHB <cihat.kose@hotmail.com>
2024-11-05 22:43:07 +0100

    Update Q82

--- src/day42/Q82.java
+++ src/day42/Q82.java
@@ -1,9 +1,7 @@
 package day42;
 
-public class S82 {
-
+public class Q82 {
     public static void main(String[] args) {
-
         String[][] chs = new String[2][]; // 2 satır , sutunlar belli değil
         chs[0] = new String[2];  //2 tane sütun  , 0.satır 2 sutun olsun
         chs[1] = new String[5];  //5 tane sütun ,  1.satır 5 sütun olsun
