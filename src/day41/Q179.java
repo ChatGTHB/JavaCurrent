ea6d95d17e2cd2203f54c48b369aaeaf7f99cc6a
ChatGTHB <cihat.kose@hotmail.com>
2024-11-07 21:25:50 +0100

    Update Q179

--- src/day41/Q179.java
+++ src/day41/Q179.java
@@ -1,19 +1,14 @@
 package day41;
 
-public class S179 {
+public class Q179 {
     public static void main(String[] args) {
-
         int[] numbers = {12, 13, 14, 15, 16};
-
         int[] keys = findMax(numbers);
     }
 
     static int[] findMax(int[] max) {
-
         int[] keys = new int[3];
-
         // kodlar yazılmıs
-
         return keys;
     }
 }
