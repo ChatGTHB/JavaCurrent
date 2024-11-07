7cc9799d3259ed8e9bcdd460aa2b71dcd6720d19
ChatGTHB <cihat.kose@hotmail.com>
2024-11-07 21:25:38 +0100

    Update Q164_2

--- src/day41/Q164_2.java
+++ src/day41/Q164_2.java
@@ -1,15 +1,12 @@
 package day41;
 
-public class S164_2 {
-
+public class Q164_2 {
     public static void main(String[] args) {
-
         int[] data = {2010, 2013, 2014, 2015, 2014};
         int key = 2014;
         int count = 0;
 
         for (int e : data) {
-
             if (e != key) {  // 2014 den farklı olduğunda continue
                 continue;
             }
