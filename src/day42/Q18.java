7fe830fb19fd803e4ddc9752ee225889b79f36fe
ChatGTHB <cihat.kose@hotmail.com>
2024-11-11 22:11:24 +0100

    Update Q18.java

--- src/day42/Q18.java
+++ src/day42/Q18.java
@@ -2,23 +2,23 @@ package day42;
 
 public class Q18 {
     public static void main(String[] args) {
-        String[][] chs = new String[5][2]; // dizinin adı.length :satır sayısı
-        chs[0] = new String[2];  // 0 ı yeniledim
-        chs[1] = new String[5];  // 1 yeniledim
+        String[][] chs = new String[5][2];
+        chs[0] = new String[2];
+        chs[1] = new String[5];
         int i = 97;
 
-        System.out.println("chs.length = " + chs.length); //5
+        System.out.println("chs.length = " + chs.length); // Prints 5
 
-        for (int a = 0; a < chs.length; a++) {  // a 5 e kadar gider
-            for (int b = 0; b < chs.length; b++) { //b 5 e kadar gider
-                chs[a][b] = "" + i;
+        for (int a = 0; a < chs.length; a++) { // a goes up to 5
+            for (int b = 0; b < chs[a].length; b++) { // Use chs[a].length instead of chs.length
+                chs[a][b] = "" + (char)i; // Convert i to char for ASCII letters
                 i++;
             }
         }
 
         for (String[] ca : chs) {
             for (String c : ca) {
-                System.out.println(c + " ");
+                System.out.print(c + " ");
             }
             System.out.println();
         }
