254602e3a5cb3e83ef4559d95a33bea4be7e40b4
ChatGTHB <cihat.kose@hotmail.com>
2024-11-15 21:08:20 +0100

    Update Q71.java

--- src/day42/Q71.java
+++ src/day42/Q71.java
@@ -2,29 +2,21 @@ package day42;
 
 public class Q71 {
     public static void main(String[] args) {
-        // Create a 2D array with 2 rows and varying column lengths
-        int[][] arr = new int[2][];
-        arr[0] = new int[]{1, 3, 5, 7}; // Initialize the first row
-        arr[1] = new int[]{1, 3};       // Initialize the second row
 
-        // Print the number of rows
-        System.out.println("Number of rows in arr: " + arr.length);
+        int[][] arr = new int[2][4];
+        arr[0] = new int[]{1, 3, 5, 7};
+        arr[1] = new int[]{1, 3};
 
-        // Print the length of each row
-        System.out.println("Length of row 0: " + arr[0].length);
-        System.out.println("Length of row 1: " + arr[1].length);
+        System.out.println("arr.length = " + arr.length); // number of rows: 2
+        System.out.println("arr[0].length = " + arr[0].length); // length of the first (index 0) row: 4
+        System.out.println("arr[1].length = " + arr[1].length); // length of the second (index 1) row: 2
 
-        // Iterate over the rows of the array
-        for (int[] row : arr) {
-            // For each row, print elements up to the number of rows (arr.length)
-            for (int i = 0; i < arr.length; i++) {
-                // Ensure we don't access elements out of bounds
-                if (i < row.length) {
-                    System.out.print(row[i] + " ");
-                }
+        for (int[] a : arr) { // `a` first takes the 1st row, then the 2nd row
+
+            for (int i = 0; i < arr.length; i++) { // prints each row up to 2 times, i.e., index 0 and 1
+                System.out.print(a[i] + " ");
             }
-            // Uncomment the line below if you'd like to print each row on a new line
-            // System.out.println();
+            //System.out.println();
         }
     }
 }
