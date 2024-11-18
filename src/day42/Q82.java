41d738a643412963d8a0719e930e9d84c845d58e
ChatGTHB <cihat.kose@hotmail.com>
2024-11-18 21:33:53 +0100

    Update Q82.java

--- src/day42/Q82.java
+++ src/day42/Q82.java
@@ -2,20 +2,20 @@ package day42;
 
 public class Q82 {
     public static void main(String[] args) {
-        String[][] chs = new String[2][]; // 2 satır , sutunlar belli değil
-        chs[0] = new String[2];  //2 tane sütun  , 0.satır 2 sutun olsun
-        chs[1] = new String[5];  //5 tane sütun ,  1.satır 5 sütun olsun
+        String[][] chs = new String[2][]; // 2 rows, columns are not specified
+        chs[0] = new String[2];  // 2 columns, the 0th row has 2 columns
+        chs[1] = new String[5];  // 5 columns, the 1st row has 5 columns
         int i = 97;
 
-        for (int a = 0; a < chs.length; a++) {  //2
-            for (int b = 0; b < chs.length; b++) { //2
+        for (int a = 0; a < chs.length; a++) {  // Loop over 2 rows
+            for (int b = 0; b < chs.length; b++) { // Loop over 2 columns
                 chs[a][b] = "" + i;
                 i++;
             }
         }
 
-        for (String[] ca : chs) {
-            for (String c : ca) {
+        for (String[] ca : chs) { // Enhanced for loop over rows
+            for (String c : ca) { // Enhanced for loop over columns
                 System.out.print(c + " ");
             }
             System.out.println();
